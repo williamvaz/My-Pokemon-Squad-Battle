b7b1045c55ff88a60ae0ab7fb19956aa4d3a2108
William <william.vaz.will@gmail.com>
2025-09-02 16:30:24 -0300

    Atualizar

--- js/home.js
+++ js/home.js
@@ -1,7 +1,3 @@
-document.getElementById("trainerBtn").addEventListener("click", () => {
-  window.location.href = "game.html"; // ajusta se for outra tela
-});
-
-document.getElementById("customBtn").addEventListener("click", () => {
-  window.location.href = "custom.html"; // ajusta se quiser outro caminho
+document.getElementById("playBtn").addEventListener("click", () => {
+  window.location.href = "game.html";
 });
