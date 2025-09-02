262ac3724ace2ab4a6dbd3ac3ec14dc2d3aee52a
William <william.vaz.will@gmail.com>
2025-09-02 16:41:38 -0300

    Refresh

--- js/home.js
+++ js/home.js
@@ -1,3 +1,7 @@
-document.getElementById("playBtn").addEventListener("click", () => {
+// qualquer clique ou tecla leva para o game.html
+function goToGame() {
   window.location.href = "game.html";
-});
+}
+
+document.body.addEventListener("click", goToGame);
+document.body.addEventListener("keydown", goToGame);
