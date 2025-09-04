6c3147a2e7efc0790593e28a4cfd346d638110c7
William <william.vaz.will@gmail.com>
2025-09-04 15:03:13 -0300

    Geral

--- js/intervalo.js
+++ js/intervalo.js
@@ -16,14 +16,18 @@ document.addEventListener("DOMContentLoaded", () => {
     const div = document.createElement("div");
     div.classList.add("pokemon-card");
 div.innerHTML = `
-  <img src="pokemons/normal/${p.ID.padStart(4, '0')}.png"><br>
-  <strong>${p.Name}</strong><br>
-  <div class="types">
-    <img src="types/${p["Type 1"]}.png" alt="${p["Type 1"]}" class="type-icon">
-    ${p["Type 2"] && p["Type 2"] !== "" ? `<img src="types/${p["Type 2"]}.png" alt="${p["Type 2"]}" class="type-icon">` : ""}
-  </div>
-  <div class="cp-label">CP: ${p.CP}</div>
-`;
+  <div class="pokemon-inner">
+    <img class="pokemon-image" src="pokemons/normal/${p.ID.padStart(4, '0')}.png" alt="${p.Name}">
+    <div class="pokemon-name">${p.Name}</div>
+    <div class="pokemon-bottom">
+      <div class="types">
+        <img src="types/${p["Type 1"]}.png" alt="${p["Type 1"]}" class="type-icon">
+        ${p["Type 2"] && p["Type 2"] !== "" ? `<img src="types/${p["Type 2"]}.png" alt="${p["Type 2"]}" class="type-icon">` : ""}
+      </div>
+      <div class="cp-label">CP: ${p.CP}</div>
+    </div>
+  </div>`;
+
     grid.appendChild(div);
   });
 });
